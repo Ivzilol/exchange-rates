88b7b32e60466b53d36be5f500bbbd5744b8129c
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-01-30 18:04:31 +0200

    Kafka Config

--- src/main/java/com/ivzilol/kafka/config/KafkaConfig.java
+++ src/main/java/com/ivzilol/kafka/config/KafkaConfig.java
@@ -11,6 +11,7 @@ import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaConfig {
+
     public static String EXCHANGE_RATE_TOPIC = "exchange_rates";
 
     @Bean
