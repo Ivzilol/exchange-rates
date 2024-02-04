71d8748bc955052e45678e0a01bc77339a635a3a
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-02-04 15:05:00 +0200

    Publish in kafka

--- src/main/java/com/ivzilol/kafka/model/ExRatesDTO.java
+++ src/main/java/com/ivzilol/kafka/model/ExRatesDTO.java
@@ -3,25 +3,7 @@ package com.ivzilol.kafka.model;
 import java.math.BigDecimal;
 import java.util.Map;
 
-public class ExRatesDTO {
+public record ExRatesDTO(String currency, Map<String, BigDecimal> rates) {
 
-    private String currency;
 
-    private Map<String, BigDecimal> rates;
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public Map<String, BigDecimal> getRates() {
-        return rates;
-    }
-
-    public void setRates(Map<String, BigDecimal> rates) {
-        this.rates = rates;
-    }
 }
