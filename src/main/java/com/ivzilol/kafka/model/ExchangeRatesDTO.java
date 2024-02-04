bf4e5cb2bb04f8a6fcb8cffa6379214ea8b36628
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-02-04 11:40:42 +0200

    Publish in kafka

--- src/main/java/com/ivzilol/kafka/model/ExchangeRatesDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ivzilol.kafka.model;
-
-import java.math.BigDecimal;
-import java.util.Map;
-
-public record ExchangeRatesDTO(String base,
-                               long timestamp,
-                               Map<String, BigDecimal> rates) {
-}
