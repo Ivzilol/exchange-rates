49d53c160308110c8c97577faefac34688a0dd8a
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-02-14 17:44:07 +0200

    Publish in kafka

--- src/main/java/com/kafka/config/KafkaConfig.java
+++ src/main/java/com/kafka/config/KafkaConfig.java
@@ -35,7 +35,6 @@ public class KafkaConfig {
     public ProducerFactory<String,Object> producerFactory(){
         return new DefaultKafkaProducerFactory<>(producerConfig());
     }
-
     @Bean
     public KafkaTemplate<String,Object> kafkaTemplate(){
         return new KafkaTemplate<>(producerFactory());
