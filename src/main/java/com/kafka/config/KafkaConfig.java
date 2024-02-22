0088e27474c36027176f82d67afa291afac11eec
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-02-22 20:35:55 +0200

    refactor

--- src/main/java/com/kafka/config/KafkaConfig.java
+++ src/main/java/com/kafka/config/KafkaConfig.java
@@ -16,7 +16,6 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
     public static String EXCHANGE_RATE_TOPIC = "exchange_rates";
-
     @Bean
     public NewTopic createTopic(){
         return new NewTopic(EXCHANGE_RATE_TOPIC, 3, (short) 1);
