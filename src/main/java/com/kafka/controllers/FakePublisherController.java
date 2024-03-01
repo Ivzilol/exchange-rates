ce5bc60e1cca9654b276ddd548d311b10a6a03e9
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-01 17:57:50 +0200

    Publish in kafka

--- src/main/java/com/kafka/controllers/FakePublisherController.java
+++ src/main/java/com/kafka/controllers/FakePublisherController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FakePublisherController {
     private final KafkaPublicationService kafkaPublicationService;
-
     public FakePublisherController(KafkaPublicationService kafkaPublicationService) {
         this.kafkaPublicationService = kafkaPublicationService;
     }
