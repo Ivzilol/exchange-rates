ef4111ceafa58664dea8b142467e279b93f8c705
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-04 09:42:20 +0200

    Publish in kafka

--- src/main/java/com/kafka/config/KafkaConfig.java
+++ src/main/java/com/kafka/config/KafkaConfig.java
@@ -33,6 +33,7 @@ public class KafkaConfig {
                 JsonSerializer.class);
         return props;
     }
+
     @Bean
     public ProducerFactory<String,Object> producerFactory(){
         return new DefaultKafkaProducerFactory<>(producerConfig());
